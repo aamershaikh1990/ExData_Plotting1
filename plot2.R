f911033b4e95279846c60d0ee5e8cd897cba3825
aamershaikh1990 <aamershaikh1990@gmail.com>
2014-08-10 10:10:50 +0530

    new file

---
+++ plot2.R
@@ -0,0 +1,6 @@
+source("prep.R")
+
+tbl <- prepareData()
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+plot(tbl$DateTime,tbl$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
