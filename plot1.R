2efcfc254d8532f0539df0d4776ca785fa08bfb5
aamershaikh1990 <aamershaikh1990@gmail.com>
2014-08-10 10:10:54 +0530

    new file

---
+++ plot1.R
@@ -0,0 +1,1 @@
+source("prep.R")tbl <- prepareData()	png(filename = "plot1.png", width = 480, height = 480, units = "px")    hist(tbl$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")    dev.off()
